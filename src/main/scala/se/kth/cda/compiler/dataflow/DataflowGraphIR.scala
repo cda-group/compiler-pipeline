769bf4855048c6c5c8bc7d54af7155fef518f602
Paris Carbone <parisc@kth.se>
2019-06-19 14:19:46 +0200

    refactoring, metainfo etc

--- src/main/scala/se/kth/cda/compiler/dataflow/DataflowGraphIR.scala
+++ src/main/scala/se/kth/cda/compiler/dataflow/DataflowGraphIR.scala
@@ -1,33 +1,37 @@
 package se.kth.cda.compiler.dataflow
 
-import se.kth.cda.arc.{ Type => ArcType }
+import se.kth.cda.arc.{Type => ArcType}
 
 object DataflowGraphIR {
 
 }
 
 trait DFGraph
-
+trait DGEdge
 trait DGNode
+trait Trace 
 
-case class Node(template: NodeTemplate, inChannels: List[Channel], outChannels: List[Channel]) extends DGNode
+case class MetaInfo(id: String, trace: Option[Trace], scope: Option[Scope])
 
-case class Source(outChannels: List[Channel]) extends DGNode
-case class Sink(inChannel: List[Channel]) extends DGNode
+case class Graph(sources: List[Source])
 
-case class Channel(channelType: ChannelType, dataType: Type, from: Node, to: Node) extends DGNode
+case class Scope(id: String, depth: Int, parent: Option[Scope])
 
-case class Scope(id: String, depth: Int, content: DFGraph) extends DGNode
+case class DefaultNode(info: MetaInfo, template: NodeTemplate, inputs: List[DGEdge], outputs: List[DGEdge]) extends DGNode
+case class Source(info: MetaInfo, out: List[DGEdge]) extends DGNode
+case class Sink(info: MetaInfo, in: List[DGEdge]) extends DGNode
+
+case class Edge(info: MetaInfo, edgeType: EdgeType, dataType: Type, from: DGNode, to: DGNode) extends DGEdge
 
 sealed trait NodeTemplate;
 
 case class Type(dataType: ArcType, key: Option[Key])
 case class Key(keyType: ArcType) // + some kind of access
 
-sealed trait ChannelType;
-object ChannelType {
-  case object Shuffle extends ChannelType;
-  case object Forward extends ChannelType;
-  case object Feedback extends ChannelType; // back edge
-  case object Broadcast extends ChannelType;
+sealed trait EdgeType;
+object EdgeType {
+  case object Shuffle extends EdgeType;
+  case object Forward extends EdgeType;
+  case object Feedback extends EdgeType; // back edge
+  case object Broadcast extends EdgeType;
 }
