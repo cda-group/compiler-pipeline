0174a3a1b55365b5c006af5a1e8347ad405b7f7c
Klas Segeljakt <klasseg@kth.se>
2019-07-22 10:39:29 +0200

    Refactor conversion pass

--- src/main/scala/se/kth/cda/compiler/dataflow/Converter.scala
+++ src/main/scala/se/kth/cda/compiler/dataflow/Converter.scala
@@ -1,75 +1,81 @@
 package se.kth.cda.compiler.dataflow
 
+import se.kth.cda.arc.syntaxtree.Type.Builder.{StreamAppender, Windower}
+import se.kth.cda.arc.syntaxtree.Type.{Stream, Struct}
 import se.kth.cda.arc.syntaxtree.{Type => ArcType, _}
+import se.kth.cda.compiler.dataflow.Analyzer._
+import se.kth.cda.compiler.dataflow.OperatorTemplate.{Flatmapper, Mapper}
+import se.kth.cda.compiler.dataflow.Transformer._
 
 import scala.util.Try
 
 object Converter {
 
-  // Arc operators are 1→N flatmaps
-  // If the Weld body merges only 1 element, it can be optimized into a map
-  // If the Weld body is commutative (pure unary function), the Arc loop may be data-parallelized
+  // Arc operators are flatmaps from 1→N channels
+  // If the Weld body has selectivity = 1, it can be optimized into a map
+  // If the Weld body is commutative (pure unary function), it can be data-parallelized
   def transform(input: AST.ArcStatements): DFG = {
-    val (args, body) = extractArgs(input.exprs.head)
-    val dfg = transformExpr(body, args)
+    val (arcParams, body) = extractArcParams(input.exprs.head)
+    val dfg = transformExpr(arcParams, body)
     dfg
   }
 
-  def extractArgs(expr: AST.Expr): (Map[String, ArcArg], AST.Expr) = {
+  def extractArcParams(expr: AST.Expr): (Map[String, ArcParam], AST.Expr) = {
     import AST.ExprKind._
 
     expr.kind match {
       case Lambda(params, body) => {
-        val args = params.zipWithIndex.flatMap { case (p, i) => ArcArg.fromParam(p, i).toOption }
-        val map = args.map(a => a.symbol.name -> a).toMap
-        (map, body)
+        val arcParams = params
+          .flatMap(ArcParam.from(_).toOption)
+          .map(a => a.symbol.name -> a)
+          .toMap
+        (arcParams, body)
       }
       case _ => ???
     }
   }
 
-  private def transformExpr(expr: AST.Expr, args: Map[String, ArcArg]): DFG = {
+  private def transformExpr(arcParams: Map[String, ArcParam], arcExpr: AST.Expr): DFG = {
     import AST.ExprKind._
     import AST.IterKind
 
-    println(args)
-
-    expr.kind match {
-      case For(iter, builder, body) =>
-        iter.kind match {
+    arcExpr.kind match {
+      // for(stream, streamappender, body)
+      case arcFor: For =>
+        arcFor.iterator.kind match {
           case IterKind.KeyByIter => ???
-          case IterKind.NextIter | IterKind.UnknownIter => // TODO don't have unkown iter here!!!
-            builder.ty match {
-              case Types.Builders.StreamAppender(elemTy, _) =>
-                body.kind match {
-                  case Lambda(params, weldBody) =>
-                    val builderName = params(0).symbol
-                    val builderInit = builder.kind.asInstanceOf[Ident].symbol
-                    val valueParam = params(2)
-
-                    val weldExpr = mapBodyToWeld(valueParam, weldBody)
-                    val weldProgram = AST.Program(macros = Nil, weldExpr, null)
-
-                    val output = args(builderInit.name).asInstanceOf[ArcArg.SinkArg]
-                    //assert(output.isInstanceOf[ArcArg.SinkArg]);
-                    val input = (iter.data.kind match {
-                      case Ident(symb) => args(symb.name)
-                      case _           => ???
-                    }).asInstanceOf[ArcArg.SourceArg]
-                    val sink = Sink.empty
-                    val e2 = Edge.forward(Type.fromArc(output.ty), null, sink)
-                    val mapper = OperatorTemplate.Mapper.withBody(weldProgram).copy(outputs = List(e2))
-                    e2.from = mapper
-                    val e1 = Edge.forward(Type.fromArc(input.ty), null, mapper)
-                    mapper.inputs = List(e1)
-                    val source = Source.withOut(e1)
-                    e1.from = source
-                    sink.in = List(e2)
-
+          case IterKind.NextIter | IterKind.UnknownIter => // TODO don't have unknown iter here!!!
+            arcFor.builder.ty match {
+              case _: StreamAppender =>
+                arcFor.body.kind match {
+                  // |streamappender, index, event| body
+                  case arcLambda: Lambda =>
+                    // TODO: Currently we assume the builder argument is an identifier
+                    val builderInit = arcFor.builder.kind.asInstanceOf[Ident].symbol
+                    val template = selectivity(arcLambda) match {
+                      // case s if s == 1 => Mapper(arcLambda.intoWeldMap()) TODO: Implement mapper transformation
+                      // case s if s <= 1 => Filter(arcLambda.intoWeldFilter()) TODO: Implement filter transformation
+                      case _ => Flatmapper(arcFor.body.toWeldFlatmap)
+                    }
+                    // TODO: This will not work for multiple operators
+                    val output = arcParams(builderInit.name).asInstanceOf[ArcParam.SinkParam]
+                    val input = arcFor.iterator.data.kind match {
+                      case Ident(symbol) => arcParams(symbol.name).asInstanceOf[ArcParam.SourceParam]
+                      case _ => ???
+                    }
+                    val source = Source()
+                    val inputEdge = Edge.forward(DataType(input.ty), from = source)
+                    val operator = Operator(template, inputs = List(inputEdge))
+                    inputEdge.to = operator
+                    val outputEdge = Edge.forward(DataType(output.ty), from = operator)
+                    operator.outputs = List(outputEdge)
+                    val sink = Sink(inputs = List(outputEdge))
+                    outputEdge.to = sink
                     Graph(List(source))
                   case _ => ???
                 }
-              case _: Types.Builders.Windower => ??? // TODO
+              case _: Struct => ???
+              case _: Windower => ??? // TODO
               case _ => ???
             }
           case _ => ???
@@ -78,31 +84,23 @@ object Converter {
     }
   }
 
-  private def mapBodyToWeld(valueParam: AST.Parameter, in: AST.Expr): AST.Expr = {
-    import AST.ExprKind._
-
-    in.kind match {
-      case Merge(builder, body) =>
-        val newSymb = AST.Symbol.unknown
-        Lambda(Vector(valueParam), Let(newSymb, body.ty, body, Ident(newSymb).toExpr(body.ty)).toExpr(body.ty))
-          .toExpr(body.ty)
-    }
-  }
 }
 
-sealed trait ArcArg {
+sealed trait ArcParam {
   def symbol: AST.Symbol
 }
 
-object ArcArg {
-  case class SourceArg(pos: Int, symbol: AST.Symbol, ty: ArcType) extends ArcArg
-  case class SinkArg(pos: Int, symbol: AST.Symbol, ty: ArcType) extends ArcArg
+object ArcParam {
 
-  def fromParam(param: AST.Parameter, pos: Int): Try[ArcArg] = Try {
+  case class SourceParam(symbol: AST.Symbol, ty: ArcType) extends ArcParam
+
+  case class SinkParam(symbol: AST.Symbol, ty: ArcType) extends ArcParam
+
+  def from(param: AST.Parameter): Try[ArcParam] = Try {
     param.ty match {
-      case stream: Types.Stream                     => SourceArg(pos, param.symbol, stream.elemTy)
-      case streamapp: Types.Builders.StreamAppender => SinkArg(pos, param.symbol, streamapp.elemTy)
-      case _                                        => ???
+      case stream: Stream => SourceParam(param.symbol, stream.elemTy)
+      case streamapp: StreamAppender => SinkParam(param.symbol, streamapp.elemTy)
+      case _ => ???
     }
   }
 }
