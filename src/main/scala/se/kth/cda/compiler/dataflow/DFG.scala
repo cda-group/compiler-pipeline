7fdf53bde6ed6cbbebda9f87a5aba4319a01e926
Klas Segeljakt <klasseg@kth.se>
2019-07-22 10:38:55 +0200

    Refactor DFG datastructures

--- src/main/scala/se/kth/cda/compiler/dataflow/DFG.scala
+++ src/main/scala/se/kth/cda/compiler/dataflow/DFG.scala
@@ -1,8 +1,10 @@
 package se.kth.cda.compiler.dataflow
 
-import se.kth.cda.arc.syntaxtree.{AST, Type => ArcType}
+import se.kth.cda.arc.syntaxtree.{AST, Type}
+import se.kth.cda.compiler.dataflow.OperatorTemplate.Mapper
 
 object DFG {
+
   import java.util.UUID
 
   def newNodeId: String = s"node-${UUID.randomUUID()}"
@@ -15,8 +17,11 @@ object DFG {
 }
 
 trait DFG
+
 trait DFGEdge
+
 trait DFGNode
+
 trait Trace
 
 case class Metadata(id: String, trace: Option[Trace] = None, scope: Option[Scope] = None)
@@ -25,59 +30,47 @@ case class Graph(sources: List[Source]) extends DFG
 
 case class Scope(id: String, depth: Int, parent: Option[Scope])
 
-case class Operator(
-    metadata: Metadata,
-    template: OperatorTemplate,
-    var inputs: List[DFGEdge] = Nil,
-    outputs: List[DFGEdge] = Nil)
-    extends DFGNode
+case class Operator(template: OperatorTemplate,
+                    inputs: List[DFGEdge] = Nil,
+                    var outputs: List[DFGEdge] = Nil,
+                    metadata: Metadata = Metadata(DFG.newNodeId))
+  extends DFGNode
 
-case class Source(info: Metadata, out: List[DFGEdge] = Nil) extends DFGNode
+case class Source(var outputs: List[DFGEdge] = Nil, info: Metadata = Metadata(DFG.newSourceId)) extends DFGNode
 
-object Source {
-  def empty: Source = Source(Metadata(DFG.newSourceId))
+case class Sink(inputs: List[DFGEdge] = Nil, info: Metadata = Metadata(DFG.newSinkId)) extends DFGNode
 
-  def withOut(edges: DFGEdge*): Source = Source(Metadata(DFG.newSourceId), out = edges.toList)
-}
-
-case class Sink(info: Metadata, var in: List[DFGEdge] = Nil) extends DFGNode
-
-object Sink {
-  def empty: Sink = Sink(Metadata(DFG.newSinkId))
-}
-
-case class Edge(info: Metadata, edgeType: EdgeType, dataType: Type, var from: DFGNode, to: DFGNode) extends DFGEdge
+case class Edge(info: Metadata, kind: EdgeKind, dataType: DataType, from: DFGNode, var to: DFGNode) extends DFGEdge
 
 object Edge {
-  def forward(dataType: Type, from: DFGNode, to: DFGNode): Edge =
-    Edge(Metadata(DFG.newEdgeId), EdgeType.Forward, dataType, from, to)
+
+  def forward(dataType: DataType, from: DFGNode = null, to: DFGNode = null): Edge =
+    Edge(Metadata(DFG.newEdgeId), EdgeKind.Forward, dataType, from, to)
 }
 
 sealed trait OperatorTemplate
 
 object OperatorTemplate {
-  case class Mapper(weldBody: AST.Program) extends OperatorTemplate
 
-  object Mapper {
+  case class Mapper(weldBody: AST.Expr) extends OperatorTemplate
 
-    def withBody(weldBody: AST.Program): Operator =
-      Operator(Metadata(DFG.newNodeId), Mapper(weldBody))
-  }
+  case class Filterer(weldBody: AST.Expr) extends OperatorTemplate
+
+  case class Flatmapper(weldBody: AST.Expr) extends OperatorTemplate
+
 }
 
-case class Type(dataType: ArcType, key: Option[Key])
+case class DataType(dataType: Type, key: Option[Key] = None)
 
-object Type {
-  def fromArc(ty: ArcType): Type = Type(ty, None)
-}
+case class Key(keyType: Type) // + some kind of access
 
-case class Key(keyType: ArcType) // + some kind of access
+sealed trait EdgeKind
 
-sealed trait EdgeType
+object EdgeKind {
 
-object EdgeType {
-  case object Shuffle extends EdgeType
-  case object Forward extends EdgeType
-  case object Feedback extends EdgeType // back edge
-  case object Broadcast extends EdgeType
+  case object Shuffle extends EdgeKind
+  case object Forward extends EdgeKind
+  case object Feedback extends EdgeKind // back edge
+  case object Broadcast extends EdgeKind
+
 }
